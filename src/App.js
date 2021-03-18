3eff8d5dbb2d3ca916e4c0f5b0fef9803bccde30
usman23499 <usamn23499@gmail.com>
2021-03-19 02:57:45 +0500

    Function Add Serverless

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [data,setData]=useState({})
+  useEffect(()=>{
+    fetch(`/.netlify/functions/hello?name=Pakistan Zindabad`)
+    .then(response=>response.json())
+    .then(data=>setData(data))
+  },[])
+  console.log(data)
   return (
     <div className="App">
      <h1>Hello World This is App deploy on Netlify</h1>
+    <h2>{data.message}</h2>
     </div>
   );
 }
